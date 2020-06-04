7eb2c4469875858c63e18a3a94fc8934c8cef819
josh fernandes <joshfernandes@mac.com>
2020-06-03 22:46:33 -0700

    prints out lines

--- dir/dir.go
+++ dir/dir.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
-	fmt.Println("this is the directory")
+	args := os.Args[1:]
+	if length := len(args); length != 2 {
+		panic("dir requires exactly 2 arguments")
+	}
+	origin, err := filepath.Abs(args[0])
+	check(err)
+	destination, _ := filepath.Abs(args[1])
+	check(err)
+	fmt.Println(origin)
+	fmt.Println(destination)
 }
