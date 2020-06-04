3a8f80e8dfd1a85e7347f6eeb5822add5cf1692d
josh fernandes <joshfernandes@mac.com>
2020-06-03 23:15:24 -0700

    added checks

--- dir/dir.go
+++ dir/dir.go
@@ -12,15 +12,39 @@ func check(err error) {
 	}
 }
 
+func getFilePath(arg string) string {
+	path, err := filepath.Abs(arg)
+	check(err)
+	removeFilePath(&path)
+	return path
+}
+
+func removeFilePath(path *string) {
+	fileInfo, err := os.Stat(*path)
+	check(err)
+	if fileInfo.Mode()&os.ModeType != os.ModeDir {
+		*path = filepath.Dir(*path)
+	}
+}
+
+func checkExists(path string) {
+	_, err := os.Stat(path)
+	check(err)
+}
+
 func main() {
 	args := os.Args[1:]
 	if length := len(args); length != 2 {
 		panic("dir requires exactly 2 arguments")
 	}
-	origin, err := filepath.Abs(args[0])
-	check(err)
-	destination, _ := filepath.Abs(args[1])
-	check(err)
+	origin := getFilePath(args[0])
+	destination := getFilePath(args[1])
+	if origin == destination {
+		panic("directories must be different")
+	}
+
+	checkExists(origin)
+
 	fmt.Println(origin)
 	fmt.Println(destination)
 }
